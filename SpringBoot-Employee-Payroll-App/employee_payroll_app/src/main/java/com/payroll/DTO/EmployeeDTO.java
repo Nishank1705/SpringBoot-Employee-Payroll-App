8f7cf7dd6413e5ebeeaa285eb7122a676b593d73
Nishank1705 <nishothers@gmail.com>
2025-03-04 17:05:22 +0530

    [Nishank Bathri] Add. Store Employee Payroll Data in Memory

---
+++ SpringBoot-Employee-Payroll-App/employee_payroll_app/src/main/java/com/payroll/DTO/EmployeeDTO.java
@@ -0,0 +1,10 @@
+package com.payroll.DTO;
+//UC-6 Use Lombok to Auto-Generate Getters and Setters for DTO
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+public class EmployeeDTO {
+    private String name;
+    private double salary;
+}
---
+++ SpringBoot-Employee-Payroll-App/employee_payroll_app/src/main/java/com/payroll/model/Employee.java
@@ -0,0 +1,30 @@
+package com.payroll.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Min(value = 1000, message = "Salary must be at least 1000")
+    private double salary;
+
+    public Employee(Long id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
